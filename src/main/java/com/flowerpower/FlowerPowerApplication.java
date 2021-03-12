56e43b6d71b9e8d85a52882c4392d69d94b60ba2
teo <teodoras.saulys@gmail.com>
2021-03-12 11:54:43 +0200

    first!

---
+++ src/main/java/com/flowerpower/FlowerPowerApplication.java
@@ -0,0 +1,13 @@
+package com.flowerpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowerpower/FlowerPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowerpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
