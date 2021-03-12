8b849e9edbf104bbf039726c2eb912334f007310
teo <teodoras.saulys@gmail.com>
2021-03-12 16:23:21 +0200

    add a ping web request method, add required dependencies for web

--- src/test/java/com/flowerpower/FlowerPowerApplicationTests.java
+++ src/test/java/com/flowerpower/FlowerPowerApplicationTests.java
@@ -1,13 +1,15 @@
 package com.flowerpower;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class FlowerPowerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { // empty test which tests whether the spring application context loads
 	}
-
 }
