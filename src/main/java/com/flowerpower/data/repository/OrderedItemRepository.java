a4446a03c00ab1560583ec04ad033c3f074de22d
teo <teodoras.saulys@gmail.com>
2021-03-29 18:33:32 +0300

    GEP-52: remove ordered item entity

--- src/main/java/com/flowerpower/data/repository/OrderedItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flowerpower.data.repository;
-
-import com.flowerpower.data.model.OrderedItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderedItemRepository extends CrudRepository<OrderedItem, Long> {
-}
