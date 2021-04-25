c6ee44cf780c592a31007385fd5f5c39db398d27
teo <teodoras.saulys@gmail.com>
2021-04-25 15:23:52 +0300

    GEP-66: add update order endpoint, processing and validation

--- src/main/java/com/flowerpower/data/model/Order.java
+++ src/main/java/com/flowerpower/data/model/Order.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,6 +22,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
