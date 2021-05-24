fe51d0ef265b376e53c118a88f3ea06b8d6c78c3
teo <teodoras.saulys@gmail.com>
2021-05-24 10:57:38 +0300

    fix imports

--- src/main/java/com/flowerpower/data/model/Item.java
+++ src/main/java/com/flowerpower/data/model/Item.java
@@ -2,7 +2,13 @@ package com.flowerpower.data.model;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Version;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
--- src/main/java/com/flowerpower/data/model/Order.java
+++ src/main/java/com/flowerpower/data/model/Order.java
@@ -6,7 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Version;
 import java.io.Serializable;
 import java.util.Date;
 
