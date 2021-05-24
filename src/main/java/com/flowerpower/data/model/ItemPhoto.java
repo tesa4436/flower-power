e8cfb62f0ca64b693d0bcb731d32885bee125298
Meta Bambalaite <meta012@gmail.com>
2021-05-24 13:37:19 +0300

    Added photo to the item

--- src/main/java/com/flowerpower/data/model/ItemPhoto.java
+++ src/main/java/com/flowerpower/data/model/ItemPhoto.java
@@ -5,11 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "item_photo")
@@ -22,5 +18,6 @@ public class ItemPhoto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Lob
     private byte[] photo;
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,17 +3,20 @@ delete from basket;
 delete from flower_order;
 delete from basket_items;
 
-insert into item (id, amount, item_type, name, price, description)
-values (1, 1, 0, 'Hiacintas',          3.15,   'Tai yra hiacintas!'),
-       (2, 0, 0, 'Rožė',               2.5,    'Tai yra rožė!'),
-       (3, 0, 0, 'Pelargonija',        3.55,   'Tai yra pelargonija!'),
-       (4, 25, 0, 'Tulpė',              3.1,    'Tai yra tulpė!'),
-       (5, 25, 0, 'Orchidėja',          4.5,    'Tai yra orchidėja!'),
-       (6, 25, 1, 'Hiacintų puokštė',   30.0,   'Tai yra hiacintų puokštė!'),
-       (7, 0, 1, 'Rožių puokštė',      35.25,  'Tai yra rožių puokštė!'),
-       (8, 25, 1, 'Pelargonijų puokštė',40.0,   'Tai yra pelargonijų puokštė!'),
-       (9, 255, 1, 'Tulpių puokštė',     50.5,   'Tai tulpių hiacintų puokštė!'),
-       (10, 0, 1, 'Orchidėjų puokštė', 25.0,   'Tai yra orchidėjų puokštė!');
+insert into item_photo (id, photo)
+values (1, file_read('..\flower-power\src\main\resources\flower_photos\hiacintas.jpg'));
+
+insert into item (id, amount, item_type, name, price, description, photo_id)
+values (1, 1, 0, 'Hiacintas',          3.15,   'Tai yra hiacintas!', 1),
+       (2, 0, 0, 'Rožė',               2.5,    'Tai yra rožė!', 1),
+       (3, 0, 0, 'Pelargonija',        3.55,   'Tai yra pelargonija!', 1),
+       (4, 25, 0, 'Tulpė',              3.1,    'Tai yra tulpė!', 1),
+       (5, 25, 0, 'Orchidėja',          4.5,    'Tai yra orchidėja!', 1),
+       (6, 25, 1, 'Hiacintų puokštė',   30.0,   'Tai yra hiacintų puokštė!', 1),
+       (7, 0, 1, 'Rožių puokštė',      35.25,  'Tai yra rožių puokštė!', 1),
+       (8, 25, 1, 'Pelargonijų puokštė',40.0,   'Tai yra pelargonijų puokštė!', 1),
+       (9, 255, 1, 'Tulpių puokštė',     50.5,   'Tai tulpių hiacintų puokštė!', 1),
+       (10, 0, 1, 'Orchidėjų puokštė', 25.0,   'Tai yra orchidėjų puokštė!', 1);
 
 insert into basket (basket_id) values (1);
 
